f1082cf28d23bcf6956f24c1b3c4d52941240d8f
Anthony Vicente <anthony@mvicente.com>
2018-03-20 21:22:01 -0500

    finished navbar javascript implementation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
+function menuDropDown() {
+    var navListItems = $('.navList').children('.navListItem').not('.menuButton');
+    for (var i = 0; i < navListItems.length; i++) {
+        $(navListItems[i]).delay(i * 50).fadeIn(100);
+    }
+}
+
+function menuDropUp() {
+    var navListItems = $('.navList').children('.navListItem').not('.menuButton');
+    for (var i = (navListItems.length - 1); i >= 0; i--) {
+        $(navListItems[i]).delay((navListItems.length * 50) - (i * 50)).fadeOut(100);
+    }
+}
+
 window.addEventListener('load', function() {
+    if ($(document).width() <= 500) {
+        $('.menuButton').on('mouseover', menuDropDown);
+        $('.navList').on('mouseleave', menuDropUp);
+    }
+});
 
-    $('.menuButton').on('mouseover', function() {
-        var navListItems = $('.navList').children('.navListItem').not('.menuButton');
-        for (var i = 0; i < navListItems.length; i++) {
-            $(navListItems[i]).delay(i * 50).fadeIn(100);
-        }
-    });
-
-    $('.navList').on('mouseleave', function() {
-        var navListItems = $('.navList').children('.navListItem').not('.menuButton');
-        for (var i = (navListItems.length - 1); i >= 0; i--) {
-            $(navListItems[i]).delay((navListItems.length * 50) - (i * 50)).fadeOut(100);
-        }
-    })
+window.addEventListener('resize', function() {
+    if ($(document).width() <= 500) {
+        $('.navList').children('.navListItem').not('.menuButton').hide();
+        $('.menuButton').on('mouseover', menuDropDown);
+        $('.navList').on('mouseleave', menuDropUp);
+    } else {
+        menuDropDown();
+        $('.menuButton').off();
+        $('.navList').off();
+    }
 });

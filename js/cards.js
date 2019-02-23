c5e5d3db1fe0e7ff0d19ddbd0d9247088752313b
Anthony Vicente <anthony@mvicente.com>
2019-02-23 17:47:05 -0600

    implements targeting last card for special styles to keep in place

--- js/cards.js
+++ js/cards.js
@@ -1,7 +1,12 @@
 function initializeCards (row) {
+    // get the card elements from the row and convert into an array of elements
     const cards = Array.prototype.slice.call(row.getElementsByClassName('card'));
     const firstCard = cards[0];
+    // we don't need an event listener on the first card
     cards.shift();
+
+    const lastCard = cards[cards.length - 1];
+    cards.splice(-1,1);
 
     for (let card of cards) {
         card.addEventListener('mouseover', () => {
@@ -11,6 +16,13 @@ function initializeCards (row) {
             firstCard.className = firstCard.className.replace(' shifted-card', '');
         });
     }
+
+    lastCard.addEventListener('mouseover', () => {
+        firstCard.className += ' shifted-card--last';
+    });
+    lastCard.addEventListener('mouseout', () => {
+        firstCard.className = firstCard.className.replace(' shifted-card--last', '');
+    });
 }
 
 const horizontalRow = document.getElementsByClassName('horizontal-card-row')[0];

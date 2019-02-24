6fcfc8666662a6c38f8a50eaa3142cc50f27d7b7
Anthony Vicente <anthony@mvicente.com>
2019-02-24 16:24:53 -0600

    adjusts css to more closely match examples. adds source links for later implementation

--- js/cards.js
+++ js/cards.js
@@ -1,13 +1,14 @@
-function initializeCards (row) {
+function initializeCards(row) {
     // get the card elements from the row and convert into an array of elements
     const cards = Array.prototype.slice.call(row.getElementsByClassName('card'));
     const firstCard = cards[0];
-    // we don't need an event listener on the first card
-    cards.shift();
-
     const lastCard = cards[cards.length - 1];
-    cards.splice(-1,1);
 
+    // we dont need the standard mouseover listeners on either the first or last cards
+    cards.shift();
+    cards.splice(-1, 1);
+
+    // need to add listeners to each card that adds/removes a class to/from the first card
     for (let card of cards) {
         card.addEventListener('mouseover', () => {
             firstCard.className += ' shifted-card';
@@ -17,6 +18,7 @@ function initializeCards (row) {
         });
     }
 
+    // the last card adds a special class to the first card
     lastCard.addEventListener('mouseover', () => {
         firstCard.className += ' shifted-card--last';
     });
@@ -29,3 +31,4 @@ const horizontalRow = document.getElementsByClassName('horizontal-card-row')[0];
 initializeCards(horizontalRow);
 const verticalRow = document.getElementsByClassName('vertical-card-row')[0];
 initializeCards(verticalRow);
+
